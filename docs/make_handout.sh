20b4c34d97abfd8b59269c23a163032a9fc8492e
matt venn <matt@mattvenn.net>
2014-10-07 11:31:33 +0100

    new resources

--- docs/make_handout.sh
+++ docs/make_handout.sh
@@ -1,4 +1,4 @@
 #regen pdf
 inkscape handout-side2.svg --export-pdf handout-side2.pdf
 #stick both together
-pdftk A=handout-side1.pdf B=handout-side2.pdf cat A B1-endleft output handout.pdf
+pdftk A=handout-side1.pdf B=handout-side2.pdf cat A B1-end output handout.pdf
